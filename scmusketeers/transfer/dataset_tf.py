7d4b9f55860ba5d4a214a744672e6e502b567078
Christophe Becavin <becavin@ipmc.cnrs.fr>
2025-05-22 00:18:14 +0200

    Prepare fix hp

--- scmusketeers/transfer/dataset_tf.py
+++ scmusketeers/transfer/dataset_tf.py
@@ -392,3 +392,428 @@ def split_train_val(workflow):
 
     print("dataset has been preprocessed")
     workflow.dataset.create_inputs()
+
+    def train_split(
+        self,
+        mode=None,
+        pct_split=None,
+        obs_key=None,
+        n_keep=None,
+        split_strategy="random",
+        keep_obs=None,
+        obs_subsample=None,
+        train_test_random_seed=None,
+    ):
+        """
+        Splits train and val datasets according to several modalities.
+        percentage : Classic train test split
+            pct_split : proportion (between 0 and 1) of the dataset to use as train
+            split_strategy : Method/metric to use to determine which cells to chose from. Currently supported is 'random
+        entire_condition : Splits by keeping certain batches of obs_key in train and others in val
+            obs_key : in task2, one of adata.obs. Key to use to split the batch
+            keep_obs : list of observations of obs_key to keep in train
+        fixed_number : Symmetrical subsampling. We keep n_keep cells of each class of obs_key condition. Number of cells in training set will be equal to n_keep * adata.obs[obs_key].unique()
+            obs_key : in task3, one of adata.obs. Key to use to select subsample of cells
+            n_keep : number of cells to keep for each class.
+            split_strategy : Method/metric to use to determine which cells to chose from. Currently supported is 'random
+        Asymetrical_subsampling : We subsample one class while keeping the other ones intact. We keep n_keep cells for the obs_subsample class of obs_key.
+            obs_key : in task3, one of adata.obs. Key to use to select subsample of cells
+            obs_subsample : class of obs_key to subsample
+            n_keep : number of cells to keep
+        """
+        self.mode = mode
+        self.train_test_random_seed = train_test_random_seed
+        if split_strategy == "avg_marker_ranking":
+            markers = load_ref_markers(
+                self.adata_train_extended, marker_path=self.markers_path
+            )
+            if obs_key:
+                self.obs_key = obs_key
+                avg_scores = marker_ranking(
+                    markers,
+                    adata=self.adata_train_extended,
+                    obs_key=self.obs_key,
+                )
+            else:
+                avg_scores = marker_ranking(
+                    markers,
+                    adata=self.adata_train_extended,
+                    obs_key=self.class_key,
+                )
+        if split_strategy == "sum_marker_score":
+            markers = load_ref_markers(
+                self.adata_train_extended, marker_path=self.markers_path
+            )
+            if obs_key:
+                self.obs_key = obs_key
+                sum_scores = sum_marker_score(
+                    markers,
+                    adata=self.adata_train_extended,
+                    obs_key=self.obs_key,
+                )
+            else:
+                sum_scores = sum_marker_score(
+                    markers,
+                    adata=self.adata_train_extended,
+                    obs_key=self.class_key,
+                )
+        if mode == "percentage":
+            self.pct_split = pct_split
+            print(self.adata_train_extended.obs[self.class_key].value_counts())
+            if split_strategy == "random" or not split_strategy:
+                # Dirty workaround for celltypes with 1 cell only, which is a rare case
+                stratification = self.adata_train_extended.obs[
+                    self.class_key
+                ].copy()
+                if min(stratification.value_counts()) == 1:
+                    solo_ct = list(
+                        stratification.value_counts()[
+                            stratification.value_counts() == 1
+                        ].index
+                    )
+                    max_ct = list(
+                        stratification.value_counts()[
+                            stratification.value_counts()
+                            == stratification.value_counts().max()
+                        ].index
+                    )[0]
+                    mapping = {}
+                    for i in solo_ct:
+                        w = np.where(stratification == i)[0]
+                        stratification.iloc[w] = max_ct
+                        mapping[i] = w
+                train_idx, val_idx = train_test_split(
+                    np.arange(self.adata_train_extended.n_obs),
+                    train_size=self.pct_split,
+                    stratify=stratification,
+                    random_state=self.train_test_random_seed,
+                )  # split on the index
+                if min(stratification.value_counts()) == 1:
+                    for ct, w in mapping.items():
+                        # Adding the single celltype to the training dataset.
+                        if not w in train_idx:
+                            train_idx = np.append(train_idx, w)
+                            val_idx = val_idx[val_idx != w]
+            if split_strategy == "avg_marker_ranking":
+                val_idx = []
+                for ct in self.adata_train_extended.obs[
+                    self.class_key
+                ].unique():
+                    sub_adata = self.adata_train_extended[
+                        self.adata_train_extended.obs[self.class_key] == ct, :
+                    ]
+                    val_idx += list(
+                        sub_adata.obs["ranking_marker_average"]
+                        .sort_values()
+                        .tail(int(sub_adata.n_obs * (1 - self.pct_split)))
+                        .index
+                    )  # select bottom 1-pct % to use as val
+            if split_strategy == "sum_marker_score":
+                val_idx = []
+                for ct in self.adata_train_extended.obs[
+                    self.class_key
+                ].unique():
+                    sub_adata = self.adata_train_extended[
+                        self.adata_train_extended.obs[self.class_key] == ct, :
+                    ]
+                    val_idx += list(
+                        sub_adata.obs["sum_marker_score"]
+                        .sort_values()
+                        .tail(int(sub_adata.n_obs * (1 - self.pct_split)))
+                        .index
+                    )  # select bottom 1-pct % to use as val
+
+            spl = pd.Series(
+                ["train"] * self.adata_train_extended.n_obs,
+                index=self.adata_train_extended.obs.index,
+            )
+            spl.iloc[val_idx] = "val"
+            self.adata_train_extended.obs["train_split"] = spl.values
+        elif mode == "entire_condition":
+            self.obs_key = obs_key
+            self.keep_obs = keep_obs
+            print(
+                f"splitting this adata train/val : {self.adata_train_extended}"
+            )
+            keep_idx = self.adata_train_extended.obs[obs_key].isin(
+                self.keep_obs
+            )
+            to_keep = pd.Series(
+                ["val"] * self.adata_train_extended.n_obs,
+                index=self.adata_train_extended.obs.index,
+            )
+            to_keep[keep_idx] = "train"
+            self.adata_train_extended.obs["train_split"] = to_keep
+        elif mode == "fixed_number":
+            self.obs_key = obs_key
+            self.n_keep = n_keep
+            keep_idx = []
+            for obs_class in self.adata_train_extended.obs[
+                self.obs_key
+            ].unique():
+                if split_strategy == "random":
+                    n_keep = min(
+                        self.adata_train_extended.obs[
+                            self.obs_key
+                        ].value_counts()[obs_class],
+                        self.n_keep,
+                    )  # For celltypes with nb of cells < n_keep, we keep every cells
+                    keep = list(
+                        self.adata_train_extended[
+                            self.adata_train_extended.obs[self.obs_key]
+                            == obs_class
+                        ]
+                        .obs.sample(
+                            n_keep, random_state=self.train_test_random_seed
+                        )
+                        .index
+                    )
+                if split_strategy == "avg_marker_ranking":
+                    sub_adata = self.adata_train_extended[
+                        self.adata_train_extended.obs[self.obs_key]
+                        == obs_class,
+                        :,
+                    ]
+                    keep = list(
+                        sub_adata.obs["ranking_marker_average"]
+                        .sort_values()
+                        .head(self.n_keep)
+                        .index
+                    )  # For celltypes with nb of cells < n_keep, we keep every cells
+                if split_strategy == "sum_marker_score":
+                    sub_adata = self.adata_train_extended[
+                        self.adata_train_extended.obs[self.obs_key]
+                        == obs_class,
+                        :,
+                    ]
+                    keep = list(
+                        sub_adata.obs["sum_marker_score"]
+                        .sort_values()
+                        .head(self.n_keep)
+                        .index
+                    )
+                keep_idx += keep
+            to_keep = pd.Series(
+                ["val"] * self.adata_train_extended.n_obs,
+                index=self.adata_train_extended.obs.index,
+            )
+            to_keep[keep_idx] = "train"
+            self.adata_train_extended.obs["train_split"] = to_keep
+        elif mode == "Asymetrical_subsampling":
+            self.obs_key = obs_key
+            self.obs_subsample = obs_subsample
+            self.n_keep = n_keep
+            n_remove = (
+                self.adata_train_extended[
+                    self.adata_train_extended.obs[self.class_key]
+                    == self.obs_subsample
+                ].n_obs
+                - self.n_keep
+            )
+            remove_idx = (
+                self.adata_train_extended[
+                    self.adata_train_extended.obs[self.class_key]
+                    == self.obs_subsample
+                ]
+                .obs.sample(n_remove, random_state=self.train_test_random_seed)
+                .index
+            )
+            to_keep = pd.Series(
+                ["train"] * self.adata_train_extended.n_obs,
+                index=self.adata_train_extended.obs.index,
+            )
+            to_keep[remove_idx] = "val"
+            self.adata_train_extended.obs["train_split"] = to_keep
+        else:
+            print(f"{mode} is not a valid splitting mode")
+            return
+
+        train_split = self.adata.obs[self.test_split_key].astype(
+            "str"
+        )  # Replace the test, train, val values in the global adata object
+        train_split[self.adata_train_extended.obs_names] = (
+            self.adata_train_extended.obs["train_split"]
+        )
+        self.adata.obs["train_split"] = train_split
+
+        print(
+            f'train, test, val proportions : {self.adata.obs["train_split"].value_counts()}'
+        )
+
+    def create_inputs(self):
+        """
+        Must be called after train_split
+        """
+        self.adata_train = self.adata[
+            self.adata.obs["train_split"] == "train"
+        ].copy()
+        self.adata_val = self.adata[
+            self.adata.obs["train_split"] == "val"
+        ].copy()
+        self.adata_test = self.adata[
+            self.adata.obs["train_split"] == "test"
+        ].copy()
+        self.X = densify(self.adata.X)
+        self.X_train = densify(self.adata_train.X)
+        self.X_val = densify(self.adata_val.X)
+        self.X_test = densify(self.adata_test.X)
+        self.y = self.adata.obs[f"true_{self.class_key}"]
+        self.y_train = self.adata_train.obs[f"true_{self.class_key}"]
+        self.y_val = self.adata_val.obs[f"true_{self.class_key}"]
+        self.y_test = self.adata_test.obs[f"true_{self.class_key}"]
+        self.ohe_celltype = OneHotEncoder(
+            handle_unknown="ignore"
+        )  # TODO : handle the case where there can be unknown celltypes in val, pex adding an output node for 'UNK'
+        y = np.array(self.y_train).reshape(-1, 1)
+        self.ohe_celltype.fit(y)
+        self.y_one_hot = (
+            self.ohe_celltype.transform(np.array(self.y).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.y_train_one_hot = (
+            self.ohe_celltype.transform(np.array(self.y_train).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.y_val_one_hot = (
+            self.ohe_celltype.transform(np.array(self.y_val).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.y_test_one_hot = (
+            self.ohe_celltype.transform(np.array(self.y_test).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.batch = self.adata.obs[self.batch_key]
+        self.batch_train = self.adata_train.obs[self.batch_key]
+        self.batch_val = self.adata_val.obs[self.batch_key]
+        self.batch_test = self.adata_test.obs[self.batch_key]
+        self.ohe_batches = OneHotEncoder()
+        batch_ID = np.array(self.batch).reshape(
+            -1, 1
+        )  # We know batches from the whole dataset so we fit on the entire dataset
+        self.ohe_batches.fit_transform(batch_ID).astype(float).todense()
+        self.batch_one_hot = (
+            self.ohe_batches.transform(np.array(self.batch).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.batch_train_one_hot = (
+            self.ohe_batches.transform(
+                np.array(self.batch_train).reshape(-1, 1)
+            )
+            .astype(float)
+            .todense()
+        )
+        self.batch_val_one_hot = (
+            self.ohe_batches.transform(np.array(self.batch_val).reshape(-1, 1))
+            .astype(float)
+            .todense()
+        )
+        self.batch_test_one_hot = (
+            self.ohe_batches.transform(
+                np.array(self.batch_test).reshape(-1, 1)
+            )
+            .astype(float)
+            .todense()
+        )
+        self.sf = self.adata.obs["size_factors"].values  # .reshape(-1,1)
+        self.sf_train = self.adata_train.obs[
+            "size_factors"
+        ].values  # .reshape(-1,1)
+        self.sf_val = self.adata_val.obs[
+            "size_factors"
+        ].values  # .reshape(-1,1)
+        self.sf_test = self.adata_test.obs[
+            "size_factors"
+        ].values  # .reshape(-1,1)
+
+    def fake_annotation(
+        self,
+        true_celltype,
+        false_celltype,
+        pct_false,
+        train_test_random_seed=None,
+    ):
+        """
+        Creates fake annotation by modifying true_celltype to false_celltype only in adata_train. Changes pct_false cells to the wrong label
+            true_celltype : celltype to fake/rig from
+            false_celltype : celltype to fake/rig to
+            pct_false : percentage of true false_celltype to keep
+        """
+        self.true_celltype = true_celltype
+        self.false_celltype = false_celltype
+        self.pct_false = pct_false
+        true_series = self.adata_train.obs[self.class_key][
+            self.adata_train.obs[self.class_key] == self.true_celltype
+        ]
+        false_series = self.adata_train.obs[self.class_key][
+            self.adata_train.obs[self.class_key] == self.false_celltype
+        ]
+        n = len(true_series)
+        true_series = shuffle(true_series, random_state=train_test_random_seed)
+        false_idx = true_series[: round(n * self.pct_false)].index
+        true_idx = true_series[round(n * self.pct_false) :].index
+        obs_series_true = self.adata_train.obs[
+            f"true_{self.class_key}"
+        ].astype(
+            "str"
+        )  ## The true labels
+        obs_series = self.adata_train.obs[self.class_key].astype(
+            "str"
+        )  # The training labels, which include nan and faked
+        # print(f'1 {obs_series_true.value_counts()}')
+        # print(f'2 {obs_series_true[false_idx]}')
+        # print(f'2.5 {false_idx}')
+        # print(f'2.75 {len(false_idx)}')
+        obs_series_true[false_idx] = self.false_celltype
+        obs_series[false_idx] = self.false_celltype
+        # print(f'3 {obs_series_true.value_counts()}')
+        self.adata_train.obs[self.class_key] = obs_series
+        adata_obs = self.adata.obs[f"true_{self.class_key}"].astype(
+            "str"
+        )  # The true labels
+        adata_obs_train = self.adata.obs[self.class_key].astype(
+            "str"
+        )  # The training labels, which should include nan and faked
+        # print(f'4 {adata_obs.value_counts()}')
+        # print(f'adata_obs.index : {adata_obs.index}')
+        # print(f'obs_series.index : {obs_series.index}')
+        adata_obs.loc[obs_series_true.index] = (
+            obs_series_true  # Les valeurs trafiquées
+        )
+        adata_obs_train.loc[obs_series.index] = (
+            obs_series  # Les valeurs trafiquées
+        )
+        # print(f"il y a {len(true_series)} True ({self.true_celltype}) et {len(false_series)} False ({self.false_celltype})")
+        # print(f"on fake donc {len(false_idx)} cellules avec un pct_false de {self.pct_false}")
+        # print(f"adata_obs  (faked) : {adata_obs.value_counts()}")
+        # print(f"true celltype : {self.adata.obs[f'true_{self.class_key}']}")
+        self.adata.obs[f"fake_{self.class_key}"] = adata_obs
+        self.adata.obs[self.class_key] = adata_obs_train
+        self.adata.obs["faked"] = (
+            self.adata.obs[f"fake_{self.class_key}"]
+            != self.adata.obs[f"true_{self.class_key}"]
+        )
+        self.adata.obs["faked_color"] = self.adata.obs["faked"].replace(
+            {True: "faked", False: "not faked"}
+        )
+        # print(self.adata.obs['faked'].value_counts())
+
+    def small_clusters_totest(self):
+        inf_n_cells = ~self.adata_train.obs[self.class_key].isin(
+            self.adata_train.obs[self.class_key]
+            .value_counts()[
+                self.adata_train.obs[self.class_key].value_counts()
+                > self.n_perm
+            ]
+            .index
+        )  # Celltypes with too few cells in train are transferred to test
+        self.adata_train = self.adata_train[~inf_n_cells].copy()
+        inf_n_cells = inf_n_cells[inf_n_cells].index
+        self.adata.obs.loc[inf_n_cells, "train_split"] = "test"
+        self.adata_test = self.adata[
+            self.adata.obs["train_split"] == "test"
+        ].copy()
