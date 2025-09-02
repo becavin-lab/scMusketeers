ea8975247eca3ca05fab9f3d5f27ae01627999dc
Christophe Bécavin <becavin.christophe@gmail.com>
2025-09-02 21:56:56 +0200

    Run hp optimization

--- experiment_script/benchmark/00_hp_optim_all_dataset.sh
+++ experiment_script/benchmark/00_hp_optim_all_dataset.sh
@@ -1,17 +1,64 @@
-#!/bin/sh
-#
-# for dataset in  "dominguez_2022_lymph" "tosti_2021"; #   #"tran_2021" "tabula_2022_spleen"   # "yoshida_2021"
-# do
-#     sbatch --partition=gpu --gres=gpu:1 --time=35:00:00 --job-name hp_$dataset 01_hp_optim.sh $dataset Original_annotation batch
-# done
+### Sc-Musketeers directory parameters
+#working_dir="/workspace/cell/scMusketeers"
+#working_dir="/data/analysis/data_becavin/scMusketeers"
+working_dir="/data/analysis/data_becavin/scMusketeers-data"
+scmusk_path="/data/analysis/data_becavin/scMusketeers"
+#scmusk_path=$working_dir
 
-# for dataset in  "litvinukova_2020" ; #"koenig_2022"; #"lake_2021" # "dominguez_2022_spleen" #  ; # 
-# do
-#     sbatch --partition=gpu --gres=gpu:1  --nodelist=gpu03 --time=36:00:00 --job-name hp_$dataset 01_hp_optim.sh $dataset Original_annotation batch
-# done
 
-#sbatch --partition=gpu --gres=gpu:1 --output=logs/ajrccm_by_batch_hyperparam.log --time=35:00:00 --job-name hp_ajrccm_by_batch 01_hp_optim.sh ajrccm_by_batch celltype manip
-dataset="htap"
-sbatch --partition=gpu --gres=gpu:1 --output=logs/${dataset}_hyperparam.log --time=35:00:00 --job-name hp_${dataset} 01_hp_optim.sh ${dataset} ann_finest_level donor
-# sbatch --partition=gpu --gres=gpu:1 --nodelist=gpu03 --time=35:00:00 --job-name hp_hlca_par_dataset_harmonized 01_hp_optim.sh hlca_par_dataset_harmonized ann_finest_level dataset
-# sbatch --partition=gpu --gres=gpu:1 --nodelist=gpu03 --time=35:00:00 --job-name hp_hlca_trac_dataset_harmonized 01_hp_optim.sh hlca_trac_dataset_harmonized ann_finest_level dataset
+#######################################
+###         Hp_optim for hyperparaters optim
+#######################################
+task="hp_param"
+neptune_name="scmusk-hp"
+total_trial=50
+
+#### AJRCCM
+dataset_name="ajrccm_by_batch"
+class_key="celltype"
+batch_key="manip"
+hparam_path="ajrccm_r2_sch3.json"
+bestparam_path="none.csv"
+#echo "HP LOOP - ${dataset_name}"
+# nohup ./experiment_script/benchmark/00_hp_optim.sh ${working_dir} ${scmusk_path} ${task} ${neptune_name} ${dataset_name} ${class_key} ${batch_key} ${bestparam_path} ${hparam_path} ${total_trial} > experiment_script/benchmark/logs/${dataset_name}_hyperparameters_optim.log 2>&1 &
+
+#### HTAP
+dataset_name="htap"
+class_key="ann_finest_level"
+batch_key="donor"
+hparam_path="generic_r1.json"
+bestparam_path="none.csv"
+#echo "HP LOOP - ${dataset_name}"
+#nohup ./experiment_script/benchmark/00_hp_optim.sh ${working_dir} ${scmusk_path} ${task} ${neptune_name} ${dataset_name} ${class_key} ${batch_key} ${bestparam_path} ${hparam_path} ${total_trial} > experiment_script/benchmark/logs/${dataset_name}_hyperparameters_optim.log 2>&1 &
+
+#### HLCA Trac
+dataset_name="hlca_trac_dataset_harmonized"
+class_key="ann_finest_level"
+batch_key="dataset"
+hparam_path="hlca_r3.json"
+bestparam_path="none.csv"
+#echo "HP LOOP - ${dataset_name}"
+#nohup ./experiment_script/benchmark/00_hp_optim.sh ${working_dir} ${scmusk_path} ${task} ${neptune_name} ${dataset_name} ${class_key} ${batch_key} ${bestparam_path} ${hparam_path} ${total_trial} > experiment_script/benchmark/logs/${dataset_name}_hyperparameters_optim.log 2>&1 &
+
+#### HLCA Parenchyma
+dataset_name="hlca_par_dataset_harmonized"
+class_key="ann_finest_level"
+batch_key="dataset"
+hparam_path="hlca_r3.json"
+bestparam_path="none.csv"
+#echo "HP LOOP - ${dataset_name}"
+#nohup ./experiment_script/benchmark/00_hp_optim.sh ${working_dir} ${scmusk_path} ${task} ${neptune_name} ${dataset_name} ${class_key} ${batch_key} ${bestparam_path} ${hparam_path} ${total_trial} > experiment_script/benchmark/logs/${dataset_name}_hyperparameters_optim.log 2>&1 &
+
+###### All CellTypist datasets
+# dominguez_2022_lymph" "tosti_2021"; #   #"tran_2021" "tabula_2022_spleen"   # "yoshida_2021"
+class_key="Original_annotation"
+batch_key="batch"
+hparam_path="generic_r1.json"
+bestparam_path="none.csv"
+for dataset_name in  "dominguez_2022_spleen";
+#for dataset_name in  "dominguez_2022_lymph" "dominguez_2022_spleen" "tosti_2021" "tran_2021" "tabula_2022_spleen" "yoshida_2021" "litvinukova_2020" "koenig_2022" "lake_2021" "dominguez_2022_spleen";
+do
+    echo "HP LOOP - ${dataset_name}"
+    nohup ./experiment_script/benchmark/00_hp_optim.sh ${working_dir} ${scmusk_path} ${task} ${neptune_name} ${dataset_name} ${class_key} ${batch_key} ${bestparam_path} ${hparam_path} ${total_trial} > experiment_script/benchmark/logs/${dataset_name}_hyperparameters_optim.log 2>&1 &
+done
+
